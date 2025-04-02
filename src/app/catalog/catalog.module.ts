c0ffe10abf821e706abfdb74e4eda784013e2223
sergiocastro85 <sergiocastro86@gmail.com>
2025-04-02 12:39:59 -0500

    Remove ProductListComponent and associated files from the catalog module

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -5,13 +5,11 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { CatalogComponent } from './catalog.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
-    CatalogComponent,
-    ProductListComponent
+    CatalogComponent
   ],
   imports: [
     CommonModule,
